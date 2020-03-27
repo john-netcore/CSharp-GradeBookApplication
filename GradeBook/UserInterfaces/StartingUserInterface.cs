ffb479b79dd793bd63f2a9c2eecf67b05bbfcd89
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-27 17:16:19 +0100

    Instantiate The Gradebook.

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,24 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var gradeBookType = parts[2];
+
+            BaseGradeBook gradeBook;
+
+            if (gradeBookType.ToLower() == "standard")
+            {
+                gradeBook = new StandardGradeBook(name);
+            }
+            else if (gradeBookType.ToLower() == "ranked")
+            {
+                gradeBook = new RankedGradeBook(name);
+            }
+            else
+            {
+                System.Console.WriteLine("{0} is not a supported type of gradebook, please try again", gradeBookType);
+                return;
+            }
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
